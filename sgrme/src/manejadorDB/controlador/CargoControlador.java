8ada5818be707c80b6f69d0ab2db414a1776778a
juanchichu <juan0094@gmail.com>
2016-11-06 17:56:43 -0500

    agregando metodos de cargoControlador

--- sgrme/src/manejadorDB/controlador/CargoControlador.java
+++ sgrme/src/manejadorDB/controlador/CargoControlador.java
@@ -5,10 +5,108 @@
  */
 package manejadorDB.controlador;
 
+import entidad.Cargo;
+import java.util.List;
+import manejadorDB.Interfaz.MetodosCargo;
+import manejadorDB.Sesion;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+
 /**
  *
  * @author juani
  */
-public class CargoControlador {
+public class CargoControlador implements MetodosCargo{
+
+    @Override
+    public void crear(Cargo cargo) {
+        
+        SessionFactory factory = Sesion.init();
+        if(factory!=null){
+            
+            try{
+                //crear sesion
+                Session session = factory.getCurrentSession();
+                
+                //transaccion
+                session.beginTransaction();
+                
+                //guardar aeropuerto
+                session.save(cargo);
+                
+                //commitear transaccion
+                session.getTransaction().commit();
+    
+            }catch(Exception e){
+                e.printStackTrace();
+            }finally{
+                factory.close();
+            }
+        }
+        
+        
+    }
+
+    @Override
+    public List<Cargo> todos() {
+        
+        List<Cargo> cargos = null;
+        
+        SessionFactory factory = Sesion.init();
+        if(factory!=null){
+            
+            try{
+                //crear sesion
+                Session session = factory.getCurrentSession();
+                
+                //transaccion
+                session.beginTransaction();
+                
+                //obtener lista 
+                cargos=session.createNamedQuery("Cargo.findAll").list();
+                
+                //commitear transaccion
+                session.getTransaction().commit();
+    
+            }catch(Exception e){
+                e.printStackTrace();
+            }finally{
+                factory.close();
+            }
+        }
+        
+        return cargos;
+    }
+
+    @Override
+    public int cantidad() {
+        List<Cargo> cargos = null;
+        
+        SessionFactory factory = Sesion.init();
+        if(factory!=null){
+            
+            try{
+                //crear sesion
+                Session session = factory.getCurrentSession();
+                
+                //transaccion
+                session.beginTransaction();
+                
+                //obtener lista 
+                cargos=session.createNamedQuery("Cargo.findAll").list();
+                
+                //commitear transaccion
+                session.getTransaction().commit();
+    
+            }catch(Exception e){
+                e.printStackTrace();
+            }finally{
+                factory.close();
+            }
+        }
+        
+        if(cargos==null) return 0;
+        else return cargos.size();
+    }
     
 }
