adbe5ae15bc35d0960266797a8d5ab35bb08a8ce
juanchichu <juan0094@gmail.com>
2016-11-06 18:25:50 -0500

    agregando metodos de lugarControlador

--- sgrme/src/manejadorDB/controlador/LugarControlador.java
+++ sgrme/src/manejadorDB/controlador/LugarControlador.java
@@ -5,10 +5,109 @@
  */
 package manejadorDB.controlador;
 
+
+import entidad.Lugar;
+import java.util.List;
+import manejadorDB.Interfaz.MetodosLugar;
+import manejadorDB.Sesion;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+
 /**
  *
  * @author juani
  */
-public class LugarControlador {
+public class LugarControlador implements MetodosLugar {
+
+    @Override
+    public void crear(Lugar lugar) {
+        
+        SessionFactory factory = Sesion.init();
+        if(factory!=null){
+            
+            try{
+                //crear sesion
+                Session session = factory.getCurrentSession();
+                
+                //transaccion
+                session.beginTransaction();
+                
+                //guardar aeropuerto
+                session.save(lugar);
+                
+                //commitear transaccion
+                session.getTransaction().commit();
+    
+            }catch(Exception e){
+                e.printStackTrace();
+            }finally{
+                factory.close();
+            }
+        }
+        
+        
+    }
+
+    @Override
+    public List<Lugar> todos() {
+        
+        List<Lugar> lugares = null;
+        
+        SessionFactory factory = Sesion.init();
+        if(factory!=null){
+            
+            try{
+                //crear sesion
+                Session session = factory.getCurrentSession();
+                
+                //transaccion
+                session.beginTransaction();
+                
+                //obtener lista 
+                lugares=session.createNamedQuery("Lugar.findAll").list();
+                
+                //commitear transaccion
+                session.getTransaction().commit();
+    
+            }catch(Exception e){
+                e.printStackTrace();
+            }finally{
+                factory.close();
+            }
+        }
+        
+        return lugares;
+    }
+
+    @Override
+    public int cantidad() {
+        List<Lugar> lugares = null;
+        
+        SessionFactory factory = Sesion.init();
+        if(factory!=null){
+            
+            try{
+                //crear sesion
+                Session session = factory.getCurrentSession();
+                
+                //transaccion
+                session.beginTransaction();
+                
+                //obtener lista 
+                lugares=session.createNamedQuery("Lugar.findAll").list();
+                
+                //commitear transaccion
+                session.getTransaction().commit();
+    
+            }catch(Exception e){
+                e.printStackTrace();
+            }finally{
+                factory.close();
+            }
+        }
+        
+        if(lugares==null) return 0;
+        else return lugares.size();
+    }
     
 }
