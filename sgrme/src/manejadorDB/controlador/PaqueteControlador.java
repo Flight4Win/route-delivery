3152c919129028809b56d2aacb58683ed4f28b5d
juanchichu <juan0094@gmail.com>
2016-11-06 18:27:14 -0500

    agregando metodos de paqueteControlador

--- sgrme/src/manejadorDB/controlador/PaqueteControlador.java
+++ sgrme/src/manejadorDB/controlador/PaqueteControlador.java
@@ -5,10 +5,108 @@
  */
 package manejadorDB.controlador;
 
+import entidad.Paquete;
+import java.util.List;
+import manejadorDB.Interfaz.MetodosPaquete;
+import manejadorDB.Sesion;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+
 /**
  *
  * @author juani
  */
-public class PaqueteControlador {
+public class PaqueteControlador implements MetodosPaquete{
+
+    @Override
+    public void crear(Paquete paquete) {
+        
+        SessionFactory factory = Sesion.init();
+        if(factory!=null){
+            
+            try{
+                //crear sesion
+                Session session = factory.getCurrentSession();
+                
+                //transaccion
+                session.beginTransaction();
+                
+                //guardar aeropuerto
+                session.save(paquete);
+                
+                //commitear transaccion
+                session.getTransaction().commit();
+    
+            }catch(Exception e){
+                e.printStackTrace();
+            }finally{
+                factory.close();
+            }
+        }
+        
+        
+    }
+
+    @Override
+    public List<Paquete> todos() {
+        
+        List<Paquete> paquetes = null;
+        
+        SessionFactory factory = Sesion.init();
+        if(factory!=null){
+            
+            try{
+                //crear sesion
+                Session session = factory.getCurrentSession();
+                
+                //transaccion
+                session.beginTransaction();
+                
+                //obtener lista 
+                paquetes=session.createNamedQuery("Paquete.findAll").list();
+                
+                //commitear transaccion
+                session.getTransaction().commit();
+    
+            }catch(Exception e){
+                e.printStackTrace();
+            }finally{
+                factory.close();
+            }
+        }
+        
+        return paquetes;
+    }
+
+    @Override
+    public int cantidad() {
+        List<Paquete> paquetes = null;
+        
+        SessionFactory factory = Sesion.init();
+        if(factory!=null){
+            
+            try{
+                //crear sesion
+                Session session = factory.getCurrentSession();
+                
+                //transaccion
+                session.beginTransaction();
+                
+                //obtener lista 
+                paquetes=session.createNamedQuery("Paquete.findAll").list();
+                
+                //commitear transaccion
+                session.getTransaction().commit();
+    
+            }catch(Exception e){
+                e.printStackTrace();
+            }finally{
+                factory.close();
+            }
+        }
+        
+        if(paquetes==null) return 0;
+        else return paquetes.size();
+    }
     
 }
