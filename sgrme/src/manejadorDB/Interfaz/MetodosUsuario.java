b79befb0a566ca63eacc11ad842904aa72f82457
juanchichu <juan0094@gmail.com>
2016-11-06 18:04:21 -0500

    agregando metodos de clienteControlador

--- sgrme/src/manejadorDB/Interfaz/MetodosUsuario.java
+++ sgrme/src/manejadorDB/Interfaz/MetodosUsuario.java
@@ -14,5 +14,4 @@ import java.util.List;
  */
 public interface MetodosUsuario {
     public void crear(Usuario usuario);
-    public int cantidad ();
 }
--- sgrme/src/manejadorDB/controlador/ClienteControlador.java
+++ sgrme/src/manejadorDB/controlador/ClienteControlador.java
@@ -5,10 +5,109 @@
  */
 package manejadorDB.controlador;
 
+import entidad.Cargo;
+import entidad.Cliente;
+import java.util.List;
+import manejadorDB.Interfaz.MetodosCliente;
+import manejadorDB.Sesion;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+
 /**
  *
  * @author juani
  */
-public class ClienteControlador {
+public class ClienteControlador implements MetodosCliente {
+
+    @Override
+    public void crear(Cliente cliente) {
+        
+        SessionFactory factory = Sesion.init();
+        if(factory!=null){
+            
+            try{
+                //crear sesion
+                Session session = factory.getCurrentSession();
+                
+                //transaccion
+                session.beginTransaction();
+                
+                //guardar aeropuerto
+                session.save(cliente);
+                
+                //commitear transaccion
+                session.getTransaction().commit();
+    
+            }catch(Exception e){
+                e.printStackTrace();
+            }finally{
+                factory.close();
+            }
+        }
+        
+        
+    }
+
+    @Override
+    public List<Cliente> todos() {
+        
+        List<Cliente> clientes = null;
+        
+        SessionFactory factory = Sesion.init();
+        if(factory!=null){
+            
+            try{
+                //crear sesion
+                Session session = factory.getCurrentSession();
+                
+                //transaccion
+                session.beginTransaction();
+                
+                //obtener lista 
+                clientes=session.createNamedQuery("Cliente.findAll").list();
+                
+                //commitear transaccion
+                session.getTransaction().commit();
+    
+            }catch(Exception e){
+                e.printStackTrace();
+            }finally{
+                factory.close();
+            }
+        }
+        
+        return clientes;
+    }
+
+    @Override
+    public int cantidad() {
+        List<Cliente> clientes = null;
+        
+        SessionFactory factory = Sesion.init();
+        if(factory!=null){
+            
+            try{
+                //crear sesion
+                Session session = factory.getCurrentSession();
+                
+                //transaccion
+                session.beginTransaction();
+                
+                //obtener lista 
+                clientes=session.createNamedQuery("Cliente.findAll").list();
+                
+                //commitear transaccion
+                session.getTransaction().commit();
+    
+            }catch(Exception e){
+                e.printStackTrace();
+            }finally{
+                factory.close();
+            }
+        }
+        
+        if(clientes==null) return 0;
+        else return clientes.size();
+    }
     
 }
