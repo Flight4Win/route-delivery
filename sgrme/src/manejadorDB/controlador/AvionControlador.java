f5bc0186dbc864ecde0e177137051a9dffc04d27
juanchichu <juan0094@gmail.com>
2016-11-06 17:54:20 -0500

    agregando metodos de avionControlador

--- sgrme/src/manejadorDB/controlador/AvionControlador.java
+++ sgrme/src/manejadorDB/controlador/AvionControlador.java
@@ -5,10 +5,108 @@
  */
 package manejadorDB.controlador;
 
+import entidad.Avion;
+import java.util.List;
+import manejadorDB.Interfaz.MetodosAvion;
+import manejadorDB.Sesion;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+
 /**
  *
  * @author juani
  */
-public class AvionControlador {
+public class AvionControlador implements MetodosAvion {
+
+    @Override
+    public void crear(Avion avion) {
+        
+        SessionFactory factory = Sesion.init();
+        if(factory!=null){
+            
+            try{
+                //crear sesion
+                Session session = factory.getCurrentSession();
+                
+                //transaccion
+                session.beginTransaction();
+                
+                //guardar aeropuerto
+                session.save(avion);
+                
+                //commitear transaccion
+                session.getTransaction().commit();
+    
+            }catch(Exception e){
+                e.printStackTrace();
+            }finally{
+                factory.close();
+            }
+        }
+        
+        
+    }
+
+    @Override
+    public List<Avion> todos() {
+        
+        List<Avion> aviones = null;
+        
+        SessionFactory factory = Sesion.init();
+        if(factory!=null){
+            
+            try{
+                //crear sesion
+                Session session = factory.getCurrentSession();
+                
+                //transaccion
+                session.beginTransaction();
+                
+                //obtener lista 
+                aviones=session.createNamedQuery("Avion.findAll").list();
+                
+                //commitear transaccion
+                session.getTransaction().commit();
+    
+            }catch(Exception e){
+                e.printStackTrace();
+            }finally{
+                factory.close();
+            }
+        }
+        
+        return aviones;
+    }
+
+    @Override
+    public int cantidad() {
+        List<Avion> aviones = null;
+        
+        SessionFactory factory = Sesion.init();
+        if(factory!=null){
+            
+            try{
+                //crear sesion
+                Session session = factory.getCurrentSession();
+                
+                //transaccion
+                session.beginTransaction();
+                
+                //obtener lista 
+                aviones=session.createNamedQuery("Avion.findAll").list();
+                
+                //commitear transaccion
+                session.getTransaction().commit();
+    
+            }catch(Exception e){
+                e.printStackTrace();
+            }finally{
+                factory.close();
+            }
+        }
+        
+        if(aviones==null) return 0;
+        else return aviones.size();
+    }
     
 }
