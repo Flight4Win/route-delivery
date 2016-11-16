26b3bf85c9a833806866fbce39e21898bea8fa0e
Carlo <carlo.alva@pucp.pe>
2016-11-15 19:04:02 -0500

    puntos moviendose con cursor y más ciudades

--- sgrme/src/sgrme_dp1/SimulationMap.java
+++ sgrme/src/sgrme_dp1/SimulationMap.java
@@ -17,12 +17,16 @@ import controlP5.*;
 import de.fhpotsdam.utils.Integrator;
 import de.looksgood.ani.*;
 import java.util.List;
+import processing.event.MouseEvent;
 /**
  *
  * @author carlo
  */
 public class SimulationMap extends PApplet{
     
+    int num = 20;
+    ball[] balls = new ball[num];
+
     PImage mapImage = null;
     UnfoldingMap mapDay;
     UnfoldingMap mapNight;
@@ -44,6 +48,9 @@ public class SimulationMap extends PApplet{
         smooth();
         //noStroke();
 
+        for(int i = 0; i < num; i++) {
+    balls[i] = new ball((int)random(40, 560), (int)(random(0, 900)), (int)random(15, 50));
+    }
         // you have to call always Ani.init() first!
         Ani.init(this);
         
@@ -55,35 +62,12 @@ public class SimulationMap extends PApplet{
         mapNight.setZoomRange(3, 3);
         mapNight.zoomTo(3);
         
-        
-        
         MapUtils.createDefaultEventDispatcher(this, mapDay, mapNight);
         
         SimplePointMarker berlinMark=new SimplePointMarker(berlinLocation);
         SimplePointMarker dublinMark=new SimplePointMarker(dublinLocation);
         
         crearCiudades();
-        
-        /*map = new UnfoldingMap(this, new OpenStreetMap.OpenStreetMapProvider());
-        MapUtils.createDefaultEventDispatcher(this, map);
-        map.setZoomRange(2, 2);
-        map.zoomTo(2);
-        
-
-        SimpleLinesMarker connectionMarker = new SimpleLinesMarker(berlinLocation, dublinLocation);
-        
-        SimplePointMarker casaLocation=new SimplePointMarker(casa);
-        
-        
-        connectionMarker.setColor(color(255, 0, 0, 100));
-        connectionMarker.setStrokeColor(color(255, 0, 0));
-        connectionMarker.setStrokeWeight(3);
-        connectionMarker.setSelected(true);
-        casaLocation.setColor(color(255, 0, 0, 100));
-        casaLocation.setStrokeColor(color(255, 0, 0));
-        casaLocation.setStrokeWeight(1);
-
-	map.addMarkers(casaLocation,connectionMarker,berlinMark,dublinMark);*/
         
     }
     
@@ -113,13 +97,25 @@ public class SimulationMap extends PApplet{
        
 }
     public void crearCiudades(){
+
+        //Europeos
+        Location bernaLocation = new Location(46.9122256, 7.4969783);
+        Location vienaLocation = new Location(48.1158369, 16.5643864);
+        Location romaLocation = new Location(41.7996732, 12.2784015);
         
+        SimplePointMarker bernaMark=new SimplePointMarker(bernaLocation);
+        SimplePointMarker vienaMark=new SimplePointMarker(vienaLocation);
+        SimplePointMarker romaMark=new SimplePointMarker(romaLocation);
+        //Latinos
+        Location pazLocation = new Location(-16.5108495, -68.1879948);
         Location bogotaLocation = new Location(4.6983449, -74.1441489);
         Location quitoLocation = new Location(-0.121211, -78.3608112);
         Location caracasLocation = new Location(10.5977114, -67.0080677);
         Location brasiliaLocation = new Location(-23.434548, -46.4803147);
         Location limaLocation = new Location(-12.0240527,-77.1142247);
-         
+        
+        SimplePointMarker pazMark=new SimplePointMarker(pazLocation);
+        
         SimplePointMarker bogotaMark=new SimplePointMarker(bogotaLocation);
         SimplePointMarker quitoMark=new SimplePointMarker(quitoLocation);
         SimplePointMarker caracasMark=new SimplePointMarker(caracasLocation);
@@ -127,8 +123,8 @@ public class SimulationMap extends PApplet{
         SimplePointMarker limaMark=new SimplePointMarker(limaLocation);
         SimpleLinesMarker connectionMarker = new SimpleLinesMarker(brasiliaLocation, caracasLocation);
         
-        mapDay.addMarkers(bogotaMark,quitoMark,caracasMark,brasiliaMark,connectionMarker,limaMark);
-        mapNight.addMarkers(bogotaMark,quitoMark,caracasMark,brasiliaMark,connectionMarker,limaMark);
+        mapDay.addMarkers(bogotaMark,quitoMark,caracasMark,brasiliaMark,connectionMarker,limaMark,pazMark,bernaMark,vienaMark,romaMark);
+        mapNight.addMarkers(bogotaMark,quitoMark,caracasMark,brasiliaMark,connectionMarker,limaMark,pazMark,bernaMark,vienaMark,romaMark);
         
         //System.out.println(caracasMark.getScreenPosition(mapDay));
     }
@@ -163,6 +159,10 @@ public class SimulationMap extends PApplet{
                 //move((int) pos1.x,10);
                 //System.out.println();
                 //fill(0);
+                for(int i = 0; i < num; i++) {
+                    fill(125, 0, 0);
+                    balls[i].draw();
+                }
                 
     }
     
@@ -190,6 +190,30 @@ public class SimulationMap extends PApplet{
         
         ellipse(x,y,45,45);
     }
-    
 
+    public void mouseWheel(MouseEvent event) {
+  float e = event.getAmount();
+  //println(e);
+  for(int i = 0; i < num; i++) {
+    int dist = (int) (random(1, 20) * e);
+    balls[i].update(dist);
+  }
+}
+ 
+class ball {
+ public int x, y, r;
+ ball (int x_, int y_, int r_){
+  x = x_;
+  y = y_;
+  r = r_;
+ 
+ }
+  void draw() {
+    ellipse(x, y, r, r);
+  } 
+  void update(int d) {
+    Ani.to(this, (float) 1.5, "y", y+d);
+  }
+    
+}
 }
